203d39151e4a8821bc2c0b553b28f9addba6cf3c
nnikolov91 <nnikolov.91@gmail.com>
2015-10-03 01:37:47 +0200

    clean dict further

--- clean_dict.py
+++ clean_dict.py
@@ -1,12 +1,14 @@
 import nltk
-
+import enchant
 
 if __name__ == '__main__':
+    d = enchant.Dict("en_US")
     with open('data/phonemeDB.txt') as source:
         with open('data/phonemeDB_clean.txt', 'a') as target:
             for line in source:
-                tag = nltk.tag.pos_tag([line.split("\t")[0]])[0][1]
-                if tag == 'NN':
+                word = line.split("\t")[0].strip().lower()
+                tag = nltk.tag.pos_tag([word])[0][1]
+                if tag == 'NN' and d.check(word):
                     target.write(line)
 
     source.close()
